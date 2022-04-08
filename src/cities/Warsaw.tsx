77f28d995a83e99572e10cf0be9df60349a3a847
♿ <biznes@2.pl>
2022-04-08 12:16:26 +0200

    🐛 Fix Warsaw

--- src/cities/Warsaw.tsx
+++ src/cities/Warsaw.tsx
@@ -3,30 +3,28 @@ import useWebSocket from 'react-use-websocket';
 import { useMap, useMapEvents } from "react-leaflet";
 
 export default () => {
-    const map = useMap();
-    const [vehicles, setVehicles] = useState([]);
-    const [bounds, setBounds] = useState(map.getBounds());
+	const map = useMap();
+	const [vehicles, setVehicles] = useState([]);
+	const [bounds, setBounds] = useState(map.getBounds());
 
-    useWebSocket("wss://ws.domeqalt.repl.co/", {
-        onOpen: () => console.log('opened'),
-        onClose: () => console.log('closed'),
-        onMessage: ({ data }) => setVehicles(JSON.parse(data)),
-        shouldReconnect: () => true,
-        reconnectInterval: 10000,
-        reconnectAttempts: 15,
-        retryOnError: true
-    });
+	useWebSocket("wss://ws.domeqalt.repl.co/", {
+		onOpen: () => console.log('opened'),
+		onClose: () => console.log('closed'),
+		onMessage: ({ data }) => setVehicles(JSON.parse(data)),
+		shouldReconnect: () => true,
+		reconnectInterval: 10000,
+		reconnectAttempts: 15,
+		retryOnError: true
+	});
 
-    {console.log(vehicles)}
+	return <>
+		<Events />
+	</>;
 
-    return <>
-        <Events />
-    </>;
-
-    function Events() {
-        useMapEvents({
-            moveend: () => setBounds(map.getBounds())
-        });
-        return null;
-    }
+	function Events() {
+		useMapEvents({
+			moveend: () => setBounds(map.getBounds())
+		});
+		return null;
+	}
 };
