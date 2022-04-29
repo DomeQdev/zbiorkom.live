d7d5d9a043527f844ee8f84b5c8c3d8d89007963
♿ <domeqdev@pm.me>
2022-04-29 15:25:15 +0200

    settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,5 @@
 import { Close, NavigateNext } from "@mui/icons-material";
-import { Dialog, DialogContent, DialogTitle, Divider, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ToggleButton, Chip, DialogActions } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, Divider, IconButton, List, ListItemButton, ListItemIcon, ListItemText, ToggleButton, Chip, DialogActions, Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -13,12 +13,30 @@ export default () => {
         scroll="paper"
         fullScreen
     >
-        <DialogTitle style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}><span>Ustawienia</span><IconButton onClick={() => navigate("/")}><Close /></IconButton></DialogTitle>
+        <DialogTitle style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}><span>Ustawienia</span><IconButton onClick={() => navigate("../")}><Close /></IconButton></DialogTitle>
         <DialogContent dividers>
-            soon™
+            <Routes>
+                <Route path="*" element={<List>
+                    <ListItemButton onClick={() => navigate("style")}>
+                        <ListItemText primary="Styl mapy" />
+                        <NavigateNext />
+                    </ListItemButton>
+                    <Divider />
+                    <ListItemButton onClick={() => navigate("vehicles")}>
+                        <ListItemText primary="Wyświetlanie pojazdów" />
+                        <NavigateNext />
+                    </ListItemButton>
+                    <Divider />
+                    <ListItemButton onClick={() => navigate("language")}>
+                        <ListItemText primary="Język (Language)" />
+                        <NavigateNext />
+                    </ListItemButton>
+                </List>} />
+            </Routes>
         </DialogContent>
         <DialogActions>
-            soon™
+            <Button>Anuluj</Button>
+            <Button>Zapisz</Button>
         </DialogActions>
     </Dialog>;
 };
