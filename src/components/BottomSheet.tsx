62ef12c32de63d477825d26fbe05d6e8a916a44f
♿ <domeqdev@pm.me>
2022-05-04 21:10:22 +0200

    show shape

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -3,6 +3,8 @@ import { IconButton, Menu, MenuItem } from "@mui/material";
 import { BottomSheet } from "react-spring-bottom-sheet";
 import { MoreVert } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
+import { LatLngBoundsExpression } from "leaflet";
+import { useMap } from "react-leaflet";
 import { Vehicle, City, Trip } from "../util/typings";
 import StopList from "./StopList";
 import icons from "../util/icons";
@@ -10,6 +12,7 @@ import cities from "../util/cities.json";
 
 export default ({ trip, vehicle, city }: { trip?: Trip, vehicle?: Vehicle, city: City }) => {
     const navigate = useNavigate();
+    const map = useMap();
     const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
     return <BottomSheet
@@ -35,7 +38,7 @@ export default ({ trip, vehicle, city }: { trip?: Trip, vehicle?: Vehicle, city:
                     }
                 }}
             >
-                <MenuItem>Pokaż trasę</MenuItem>
+                <MenuItem onClick={() => { if(trip?.shapes) map.fitBounds(bounds(trip.shapes)); setAnchorEl(null); }}>Pokaż trasę</MenuItem>
                 {cities[city].functions.brigades && vehicle?.brigade && <MenuItem onClick={() => { navigate("brigade"); setAnchorEl(null); }}>Kursy tego pojazdu</MenuItem>}
                 {cities[city].functions.vehicleInfo && <MenuItem onClick={() => { navigate("vehicle"); setAnchorEl(null); }}>Informacje o pojeździe</MenuItem>}
             </Menu>
@@ -44,3 +47,11 @@ export default ({ trip, vehicle, city }: { trip?: Trip, vehicle?: Vehicle, city:
         {trip?.error ? <h3 style={{ textAlign: "center" }}>Nie znaleziono trasy</h3> : <StopList trip={trip} vehicle={vehicle} />}
     </BottomSheet>;
 };
+
+function bounds(shapes: [number, number][]) {
+    const minLat = Math.min(...shapes.map(x => x[0]));
+    const maxLat = Math.max(...shapes.map(x => x[0]));
+    const minLng = Math.min(...shapes.map(x => x[1]));
+    const maxLng = Math.max(...shapes.map(x => x[1]));
+    return [[minLat, minLng], [maxLat, maxLng]] as LatLngBoundsExpression;
+}
