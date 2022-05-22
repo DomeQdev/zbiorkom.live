02f2f798b75c8a548d03c99bd5f0558deba25cfc
♿ <domeqdev@pm.me>
2022-05-22 18:29:16 +0200

    Update BikeStation.tsx

--- src/pages/BikeStation.tsx
+++ src/pages/BikeStation.tsx
@@ -1,26 +1,29 @@
+import { ListItem, ListItemText, Divider } from "@mui/material";
 import { Bikes, City } from "../util/typings";
-import { translate, Translate } from "../util/Translations";
+import { translate } from "../util/Translations";
 import { useNavigate, useParams } from "react-router-dom";
 import { BottomSheet } from "react-spring-bottom-sheet";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { DirectionsBike, Refresh } from "@mui/icons-material";
+import { useMap } from "react-leaflet";
+import { DirectionsBike, PedalBike } from "@mui/icons-material";
 import BikeMarker from "../components/BikeMarker";
-import { IconButton } from "@mui/material";
 
 export default ({ bikes, city }: { bikes: Bikes[], city: City }) => {
     const navigate = useNavigate();
+    const map = useMap();
     const { id } = useParams();
     const [bike, setBike] = useState<Bikes>();
 
     useEffect(() => {
-        if(!bikes.length) return;
-        let bike = bikes.find(x => String(x.id) === id);
-        if(!bike) {
+        if (!bikes.length) return;
+        let _bike = bikes.find(x => String(x.id) === id);
+        if (!_bike) {
             toast.error(translate("bike_station_not_found"));
             return navigate(`/${city}`);
         }
-        setBike(bike);
+        if (!bike) map.setView(_bike.location, 17);
+        setBike(_bike);
     }, [bikes]);
 
     return <>
@@ -31,15 +34,25 @@ export default ({ bikes, city }: { bikes: Bikes[], city: City }) => {
             blocking={false}
             style={{ zIndex: 1000, position: "absolute" }}
             snapPoints={({ maxHeight }) => [maxHeight / 4, maxHeight * 0.6, maxHeight - 40]}
-            header={<div style={{ display: "flex", justifyContent: "space-between" }}>
-                <div />
+            header={bike && <>
                 <div style={{ display: "inline-flex", alignItems: "center" }}>
                     <span style={{ color: "white", backgroundColor: "#104a9e", borderRadius: "25px", padding: "5px", paddingLeft: "10px", paddingRight: "10px", display: "inline-flex", alignItems: "center" }}><DirectionsBike style={{ width: 18, height: 18 }} />&nbsp;{bike?.id}</span>&nbsp;{bike?.name}
                 </div>
-                <IconButton><Refresh /></IconButton>
-            </div>}
+                <br />
+                <div style={{ display: "inline-flex", alignItems: "center" }}>
+                    <PedalBike />&nbsp;<b style={{ fontSize: 18 }}>{bike.bike_racks - bike.free_racks}</b> / <span>{bike?.bike_racks}</span>
+                </div>
+            </>}
         >
-            {JSON.stringify(bike?.bikes)}
+            {bike?.bikes.sort((a, b) => a.number - b.number).map<React.ReactNode>((bik, i) => <ListItem key={`0_${i}`}>
+                <ListItemText>
+                    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                        <div>
+                            <span style={{ display: "inline-flex" }}><span style={{ color: "white", backgroundColor: "#104a9e", borderRadius: "25px", padding: "5px", paddingLeft: "10px", paddingRight: "10px", display: "inline-flex", alignItems: "center", height: 15 }}><DirectionsBike style={{ width: 18, height: 18 }} />&nbsp;{bik.number}</span>&nbsp;{bik.name}</span>
+                        </div>
+                    </div>
+                </ListItemText>
+            </ListItem>).reduce((prev, curr, i) => [prev, <Divider key={`1_${i}`} sx={{ backgroundColor: "#DCCDCD", marginLeft: "10px", marginRight: "10px" }} />, curr])}
         </BottomSheet>
     </>;
 };
