5eac94e8c8f139ddf1607280a212fe4621240e9d
♿ <domeqrecords+gh@gmail.com>
2025-06-30 12:29:59 +0200

    stop using react helmet

--- src/components/util/Helm.tsx
+++ src/components/util/Helm.tsx
@@ -1,5 +1,4 @@
 import { $Dictionary } from "i18next/typescript/helpers";
-import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import cities from "cities";
@@ -20,12 +19,12 @@ export default ({ variable, dictionary }: { variable: string; dictionary?: $Dict
     const hasDescription = description !== `${variable}.description`;
 
     return (
-        <Helmet>
+        <>
             <title>zbiorkom.live - {title}</title>
             <meta property="og:title" content={`zbiorkom.live - ${title}`} />
 
             {hasDescription && <meta name="description" content={description} />}
             {hasDescription && <meta property="og:description" content={description} />}
-        </Helmet>
+        </>
     );
 };
